e297d70fbad6ec321ffa6c6eec03444868cd0c4f
it00x <gontarz.kuba@gmail.com>
2024-01-16 23:20:14 +0100

    2

--- main.js
+++ main.js
@@ -1,54 +1,54 @@
 
 
-// function addProduct()
-// {
+function addProduct()
+{
 
-//     let productsTable=[]
+    let productsTable=[]
 
     
-//     let prod_name=document.getElementById('productName').value
-//     let prod_price=document.getElementById('productPrice').value
+    let prod_name=document.getElementById('productName').value
+    let prod_price=document.getElementById('productPrice').value
 
-//     let prod_descript=document.getElementById('Product_description').value
+    let prod_descript=document.getElementById('Product_description').value
 
-//     if (!prod_name || !prod_price)
-//     {
-//         alert('You have to fill Product Name and Product Price fields');
-//     }
+    if (!prod_name || !prod_price)
+    {
+        alert('You have to fill Product Name and Product Price fields');
+    }
 
-//     else {
-//         let new_product=
-//         {
-//             ID: productsTable.length +1,
-//             NAME: prod_name,
-//             PRICE: prod_price,
-//             DESCRIPTION: prod_descript,
-//         };
+    else {
+        let new_product=
+        {
+            ID: productsTable.length +1,
+            NAME: prod_name,
+            PRICE: prod_price,
+            DESCRIPTION: prod_descript,
+        };
 
-//         productsTable.push(new_product);
+        productsTable.push(new_product);
 
-//         document.getElementById('productName').value = '';
-//         document.getElementById('productPrice').value = '';
+        document.getElementById('productName').value = '';
+        document.getElementById('productPrice').value = '';
 
         
-//         update_Table()
-//     }
-// function update_Table()
-//     {
-//     let tbody=document.getElementById('productTable').getElementsByTagName('tbody')[0];
+        update_Table()
+    }
+function update_Table()
+    {
+    let tbody=document.getElementById('productTable').getElementsByTagName('tbody')[0];
         
-//         productsTable.forEach(
+        productsTable.forEach(
         
-//         function (product) {
-//         let new_row = document.createElement('tr')
+        function (product) {
+        let new_row = document.createElement('tr')
 
-//         new_row.innerHTML = '<td>' + product.ID + '</td>' +
-//             '<td>' + product.NAME + '</td>' +
-//             '<td>' + product.PRICE + '</td>' +
-//             '<td>' + product.DESCRIPTION + '</td>'
-//         tbody.appendChild(new_row);
-//     })
+        new_row.innerHTML = '<td>' + product.ID + '</td>' +
+            '<td>' + product.NAME + '</td>' +
+            '<td>' + product.PRICE + '</td>' +
+            '<td>' + product.DESCRIPTION + '</td>'
+        tbody.appendChild(new_row);
+    })
 
 
-//     }    
-// }
+    }    
+}
